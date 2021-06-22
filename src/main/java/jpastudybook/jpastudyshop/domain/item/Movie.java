16547b0f87fe47c30b60abaf436b7e0e00f09002
jaeyoungjang2 <lion85488@gmail.com>
2021-06-22 20:20:56 +0900

    Movie 추가

--- src/main/java/jpastudybook/jpastudyshop/domain/item/Movie.java
+++ src/main/java/jpastudybook/jpastudyshop/domain/item/Movie.java
@@ -1,2 +1,15 @@
-package jpastudybook.jpastudyshop.domain;public class Movie {
+package jpastudybook.jpastudyshop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter @Setter
+public class Movie extends Item{
+    private String director;
+    private String actor;
 }
