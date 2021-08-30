3cac047606bd0a6a7bec1c0cddb60808239b7571
jaeyoungjang2 <lion85488@gmail.com>
2021-08-30 20:51:55 +0900

    feat: payroll 추가

--- src/main/java/jpastudybook/jpastudyshop/test/PayrollDayMain.java
+++ src/main/java/jpastudybook/jpastudyshop/test/PayrollDayMain.java
@@ -1,2 +1,10 @@
-package jpastudybook.jpastudyshop.test;public class PayrollDayMain {
+import jpastudybook.jpastudyshop.test.PayrollDay;
+
+public class PayrollDayMain {
+    public static void main(String[] args) {
+        for (PayrollDay payrollDay : PayrollDay.values()) {
+            System.out.println(payrollDay);
+            System.out.println(payrollDay.pay(400, 50));
+        }
+    }
 }
